2c258ace4c08ab0f44bb0f55df61c6d6267dcb02
Boulbeba404 <boulbebaellouze@gmail.com>
2024-07-28 21:55:50 +0100

    Create apis management

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,14 +1,25 @@
 import { useState } from "react";
 import { AuthForm } from "../../components";
-
+import { AuthAPI } from "../../apis";
+import { toast } from "react-toastify";
+import { useAuth } from "../../hooks";
 
 function Login() {
+  const authApi = new AuthAPI();
+  const { setAuthStore } = useAuth();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    /// api
+    try {
+      const response = await authApi.login(email, password);
+      const { accessToken, refreshToken, userId } = response.data;
+      setAuthStore(accessToken, refreshToken, userId);
+    } catch (err) {
+      console.error(err);
+      toast.error("Erreur! Please verify your email or password");
+    }
   };
 
   return (
