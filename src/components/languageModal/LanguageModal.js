843e2f5d8ebb230d432e369e5c0d824dad0730f9
Boulbeba404 <boulbebaellouze@gmail.com>
2024-07-31 18:14:54 +0100

    Create model and users list

--- src/components/languageModal/LanguageModal.js
+++ src/components/languageModal/LanguageModal.js
@@ -13,17 +13,35 @@ const LanguageModal = ({ show, handleClose, modalType }) => {
   return (
     <Modal show={show} onHide={handleClose}>
       <Modal.Header closeButton>
-        <Modal.Title>{modalType} Language</Modal.Title>
+        <Modal.Title>{modalType} User</Modal.Title>
       </Modal.Header>
       <Modal.Body>
         <Form>
           <Form.Group className="mb-3" controlId="languageName">
-            <Form.Label>Language Name</Form.Label>
+            <Form.Label>Full Name</Form.Label>
             <Form.Control
               type="text"
               value={language.name}
               onChange={handleChange}
-              placeholder="Enter language name"
+              placeholder="Enter fullname"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="languageName">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="Email"
+              value={language.name}
+              onChange={handleChange}
+              placeholder="Enter email"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="languageName">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              value={language.name}
+              onChange={handleChange}
+              placeholder="Enter password"
             />
           </Form.Group>
         </Form>
--- src/pages/myProfile/MyProfile.js
+++ src/pages/myProfile/MyProfile.js
@@ -8,28 +8,13 @@ const MyProfile = () => {
     <Container>
       <PageTitle className="mb-3" title="My Profile" />
       <Form>
-        <Row className="mb-3">
-          <Form.Group as={Col} controlId="formGridFirstName">
-            <Form.Label>First Name</Form.Label>
-            <Form.Control type="text" placeholder="Enter first name" />
-          </Form.Group>
-
-          <Form.Group as={Col} controlId="formGridLastName">
-            <Form.Label>Last Name</Form.Label>
-            <Form.Control type="text" placeholder="Enter last name" />
-          </Form.Group>
-        </Row>
+      <Form.Group className="mb-3" controlId="formGridEmail">
+          <Form.Label>Full Name</Form.Label>
+          <Form.Control type="text" placeholder="Enter fullname" />
+        </Form.Group>
         <Form.Group className="mb-3" controlId="formGridEmail">
           <Form.Label>Email</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formGridAddress">
-          <Form.Label>Address</Form.Label>
-          <Form.Control placeholder="1234 Main St" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formGridPhone">
-          <Form.Label>Phone Number</Form.Label>
-          <Form.Control type="tel" placeholder="Enter phone number" />
         </Form.Group>
         <Button className="mb-2" type="submit">
           Save
